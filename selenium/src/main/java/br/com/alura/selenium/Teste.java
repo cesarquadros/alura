afab00b699570a846bd8c6fa5254b8c6b11e7d2c
Cesar <cesar.quadros88@gmail.com>
2019-03-14 08:15:31 -0300

    projeto leilao incluso

--- selenium/src/main/java/br/com/alura/selenium/Teste.java
+++ selenium/src/main/java/br/com/alura/selenium/Teste.java
@@ -5,24 +5,54 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class Teste {
+	
+		
+	public static void main(String... args) {
+		
+		
 
-	public static void main(String[] args) {
+		// abre firefox
+		WebDriver driver = new ChromeDriver();
 
-	    // abre firefox
-	    WebDriver driver = new ChromeDriver();
+		// acessa o site do google
+		driver.get("https://trabalhador.certponto.com.br");
 
-	    // acessa o site do google
-	    driver.get("http://localhost:9000/listaconvidados");
+		// digita no campo com nome "q" do google
+		WebElement btnAcessar = driver.findElement(By.className("btn btn-primary button-btn-access"));
+//		campoDeTexto.sendKeys("teste-selenium");
+		
+		btnAcessar.click();
+		
+		// submete o form
+//		campoDeTexto.submit();
+		
+//		boolean achou = driver.getPageSource().contains("Teste selenium");
+//		
+//		WebElement textArea = driver.findElement(By.id("text"));
+//		
+//		System.out.println(textArea.getText());
+//		
+//		if(achou) {
+//			driver.close();
+//		}
+		
+		//verificar se contem na pagina um determinado texto
+//		boolean achouNome = driver.getPageSource().contains("Ronaldo Luiz de Albuquerque");
 
-	    // digita no campo com nome "q" do google
-	    WebElement campoDeTexto = driver.findElement(By.name("nome"));
-	    WebElement campoDeTexto1 = driver.findElement(By.name("email"));
-	    WebElement campoDeTexto2 = driver.findElement(By.name("telefone"));
-	    campoDeTexto.sendKeys("Corinthians");
-	    campoDeTexto1.sendKeys("Corinthians@email");
-	    campoDeTexto2.sendKeys("111111111111");
-	    // submete o form
-	    campoDeTexto.submit();
+		// selecionar combobox
+//	    Select usuario = new Select(driver.findElement(By.name("leilao.usuario.id"))); captura o elemento
+//	    usuario.selectByVisibleText("João"); seleciona a opcao do combobox
+
+		// selecionar check box
+//	    WebElement usado = driver.findElement(By.name("leilao.usado"));
+//	    usado.click();
+
+		//pegar lista de elementos por posicao
+//		int posicao = 1; // 1o link
+//		List<WebElement> elementos = driver.findElements(By.linkText("exibir"));
+//		elementos.get(posicao - 1).click();
+
 	}
 }
